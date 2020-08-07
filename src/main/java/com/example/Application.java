b03c0091fde6f42a4efb991d9b07a104a302ad39
Navid Khalili <nkhalili@live.com>
2020-08-07 17:11:58 -0400

    Spring config with Java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,17 +1,22 @@
 package com.example;
 
+import com.example.config.ApplicationConfig;
 import com.example.service.GreetingService;
 import com.example.service.OutputService;
 import com.example.service.TimeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String[] args) throws Exception {
-        GreetingService greetingService = new GreetingService("Hello");
-        TimeService timeService = new TimeService(true);
-        OutputService outputService = new OutputService(greetingService, timeService);
+//        GreetingService greetingService = new GreetingService("Hello");
+//        TimeService timeService = new TimeService(true);
+//        OutputService outputService = new OutputService(greetingService, timeService);
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        OutputService outputService = appContext.getBean(OutputService.class);
 
         for (int i=0;i<5;i++){
-            outputService.generateOutput("Frank");
+            outputService.generateOutput("Navid");
             Thread.sleep(5000);
         }
     }
