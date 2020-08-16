5e5c9aaee018d4a58fef718774f494df81b87755
Navid Khalili <nkhalili@live.com>
2020-08-16 18:24:45 -0400

    Aspect - Pointcut syntax

---
+++ src/main/java/com/example/aspect/LoggingAspect.java
@@ -0,0 +1,13 @@
+package com.example.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Pointcut("@annotation(Loggable)")
+    public void executeLogging(){}
+}
--- src/main/java/com/example/config/ApplicationConfig.java
+++ src/main/java/com/example/config/ApplicationConfig.java
@@ -2,11 +2,13 @@ package com.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.example")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
 }
